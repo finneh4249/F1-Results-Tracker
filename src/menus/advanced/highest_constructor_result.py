26301b423f4d340430d625ce25ad8355e1d1cae0
Ethan Cornwill <ethancornwill@outlook.com>
2024-07-07 15:51:51 +1000

    added display and database queries for top 10 results

--- src/menus/advanced/highest_constructor_result.py
+++ src/menus/advanced/highest_constructor_result.py
@@ -7,8 +7,8 @@ green = Fore.GREEN
 class Menu(app_structures.BaseMenu):
     def __init__(self):
         self.options = {
-            "1": ("View Top 10 Pole Positions", self.view_driver_standings),
-            "2": ("View Top 10 Fastest Lap", self.view_race_results),
+            "1": ("View Top 10 Pole Positions", self.view_poles),
+            "2": ("View Top 10 Fastest Lap", self.view_fastest_lap),
             "3": ("View Top 10 Champions", self.view_champs),
             "4": ("View Top 10 Podiums", self.view_podiums),
             "5": ("View Top 10 Points", self.view_points),
@@ -24,37 +24,7 @@ class Menu(app_structures.BaseMenu):
         return super().get_user_choice()
     
     def call_menu(self, choice):
-        if choice == "1":
-            self.view_driver_standings()
-        elif choice == "2":
-            self.go_back()
-
-    def view_driver_standings(self):
-        app_structures.clear()
-
-        print(green + "Choose a year (1950 - 2024): \n")
-        year = input()
-
-        if int(year) < 1950 or int(year) > 2024:
-            print(Back.RED + "Invalid choice")
-            return self.view_race_results()
-
-        races = db_structures.get_race_id_by_year(int(year))
-        race_head = ["Round No.", "Race Name"]
-        print(tabulate(races, headers=race_head, tablefmt="fancy_grid"))
-        print(green + "Choose a race \n")
-        race = input()
-        results = db_structures.get_standings_after_race(int(year), int(race))
-        print(Fore.GREEN + "Getting Results...")
-        app_structures.load(2)
-        results_head = ["Position", "Driver", "Points", "Positions Gained"]
-        for r in races:
-            if str(r[0]) == race:
-                race_name = r[1]
-                app_structures.clear()
-                print(green + app_structures.title_art(f"{race_name} Race Results"))
-                break
-        print (tabulate(results, headers=results_head, tablefmt="fancy_grid"))
+        self.options[choice][1]()
 
     def go_back(self):
         menu.AdvancedMenu().run()
@@ -65,3 +35,70 @@ class Menu(app_structures.BaseMenu):
             choice = self.get_user_choice()
             if choice in self.options:
                 self.call_menu(choice)
+    
+
+    def view_poles(self):
+        app_structures.clear()
+
+        results = db_structures.get_topten_results("constructor", "total_pole_positions")
+        print(green + "Getting Results...")
+
+        app_structures.load(2)
+
+        display_results(results, "Poles")
+
+    def view_fastest_lap(self):
+        app_structures.clear()
+
+        results = db_structures.get_topten_results("constructor", "total_fastest_laps")
+        print(green + "Getting Results...")
+
+        app_structures.load(2)
+
+        display_results(results, "Fastest Laps")
+
+    def view_champs(self):
+        app_structures.clear()
+
+        results = db_structures.get_topten_results("constructor", "total_championship_wins")
+        print(green + "Getting Results...")
+
+        app_structures.load(2)
+
+        display_results(results, "Championships")
+
+    def view_podiums(self):
+        app_structures.clear()
+
+        results = db_structures.get_topten_results("constructor", "total_podiums")
+        print(green + "Getting Results...")
+
+        app_structures.load(2)
+
+        display_results(results, "Podiums")
+
+    def view_points(self):
+        app_structures.clear()
+
+        results = db_structures.get_topten_results("constructor", "total_championship_points")
+        print(green + "Getting Results...")
+
+        app_structures.load(2)
+
+        display_results(results, "Points")
+
+    def view_wins(self):
+        app_structures.clear()
+
+        results = db_structures.get_topten_results("constructor", "total_race_wins")
+        print(green + "Getting Results...")
+
+        app_structures.load(2)
+
+        display_results(results, "Race Wins")
+
+def display_results(results, title):
+    app_structures.clear()
+    print(green + app_structures.title_art(f"Top 10 {title}"))
+    print(tabulate(results, headers=["Constructor", f"{title}"], tablefmt="fancy_grid"))
+    app_structures.load(3)
