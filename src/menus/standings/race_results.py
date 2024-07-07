4034d85fa0986f97d0a6d201569c906f7ea426e8
Ethan Cornwill <ethancornwill@outlook.com>
2024-07-07 15:24:46 +1000

    fix race_results.py by changing the imports correctly throughout the code

--- src/menus/standings/race_results.py
+++ src/menus/standings/race_results.py
@@ -1,17 +1,31 @@
 from structures import db_structures, app_structures
+from menus import main_menu as menu
 from colorama import Fore, Back
 from tabulate import tabulate
 green = Fore.GREEN
 
 class RaceResults(app_structures.BaseMenu):
-    def __init__(self, ascii_title):
-        self.ascii_title = ascii_title
+    def __init__(self):
         self.options = {
             "1": ("View Race Results", self.view_race_results),
             "2": ("Go Back", self.go_back),
         }
+    def display_menu(self):
+        print("Choose a category: \n")  # Display menu options
+        for key, value in self.options.items():
+            print(Fore.GREEN + f"{key}. {value[0]}")
+
+    def get_user_choice(self):
+        return super().get_user_choice()
+    
+    def call_menu(self, choice):
+        if choice == "1":
+            self.view_race_results()
+        elif choice == "2":
+            self.go_back()
 
     def view_race_results(self):
+        app_structures.clear()
         print(green + "Choose a year (1950 - 2024): \n")
         year = input()
 
@@ -25,19 +39,29 @@ class RaceResults(app_structures.BaseMenu):
         print(green + "Choose a race \n")
         race = input()
         results = db_structures.get_race_results(int(year), int(race))
+        print(Fore.GREEN + "Getting Results...")
+        app_structures.load(2)
         results_head = ["Position", "Driver", "Constructor", "Gap to Leader", "Points", "Grid Position", "Positions Gained"]
         for r in races:
             if str(r[0]) == race:
                 race_name = r[1]
-                print(green + self.ascii_title(f"{race_name} Race Results"))
+                app_structures.clear()
+                print(green + app_structures.title_art(f"{race_name} Race Results"))
                 break
         print(tabulate(results, headers=results_head, tablefmt="fancy_grid"))
-        self.display_menu()
 
     def go_back(self):
-        app_structures.F1Menu.display_menu(self, self.options, self.ascii_title)
+        menu.F1Menu().run()
 
-    def display_menu(self):
-        app_structures.BaseMenu.display_menu(self, self.options, self.ascii_title)
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = self.get_user_choice()
+            if choice in self.options:
+                self.call_menu(choice)
+            else:
+                print(Back.RED + "Invalid choice")  # Display error message for invalid choice
+
+    
     
     
