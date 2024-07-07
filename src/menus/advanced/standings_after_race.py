2140ff5b44f97040f8c1d9569dc5c2eb12745659
Ethan Cornwill <ethancornwill@outlook.com>
2024-07-07 17:13:42 +1000

    refactor advanced features from 4 files to 2

--- src/menus/advanced/standings_after_race.py
+++ src/menus/advanced/standings_after_race.py
@@ -7,8 +7,9 @@ green = Fore.GREEN
 class Menu(app_structures.BaseMenu):
     def __init__(self):
         self.options = {
-            "1": ("View Driver Standings", self.view_driver_standings),
-            "2": ("Go Back", self.go_back),
+            "1": ("View Driver Standings", self.view_standings),
+            "2": ("View Constructor Standings", self.view_standings),
+            "3": ("Go Back", self.go_back),
         }
     def display_menu(self):
         print("Choose a category: \n")  # Display menu options
@@ -20,37 +21,49 @@ class Menu(app_structures.BaseMenu):
     
     def call_menu(self, choice):
         if choice == "1":
-            self.view_driver_standings()
-        elif choice == "2":
+            self.view_standings("driver")
+        elif choice =="2":
+            self.view_standings("constructor")
+        elif choice == "3":
             self.go_back()
 
-    def view_driver_standings(self):
+    def view_standings(self, type):
         app_structures.clear()
 
-        print(green + "Choose a year (1950 - 2024): \n")
-        year = input()
+        
+        title = type.title()
 
-        if int(year) < 1950 or int(year) > 2024:
-            print(Back.RED + "Invalid choice")
-            return self.view_race_results()
+        if type == "driver":
+            print(green + "Choose a year (1950 - 2024): \n")
+            year = input()
+            if int(year) < 1950 or int(year) > 2024:
+                print(Back.RED + "Invalid choice")
+                return self.view_standings(type)
 
+        if type == "constructor":
+            print(green + "Choose a year (1958 - 2024): \n")
+            year = input()
+            if int(year) < 1958 or int(year) > 2024:
+                print(Back.RED + "Invalid choice")
+                return self.view_standings(type)
+        
         races = db_structures.get_race_id_by_year(int(year))
         race_head = ["Round No.", "Race Name"]
         print(tabulate(races, headers=race_head, tablefmt="fancy_grid"))
         print(green + "Choose a race \n")
         race = input()
-        results = db_structures.get_standings_after_race("driver", int(year), int(race))
+        results = db_structures.get_standings_after_race(type, int(year), int(race))
         print(Fore.GREEN + "Getting Results...")
         app_structures.load(2)
-        results_head = ["Position", "Driver", "Points", "Positions Gained"]
+        results_head = ["Position", f"{title}", "Points", "Positions Gained"]
         for r in races:
             if str(r[0]) == race:
                 race_name = r[1]
                 app_structures.clear()
-                print(green + app_structures.title_art(f"{race_name} Race Results"))
+                print(green + app_structures.title_art(f"{title} Standings after {race_name}"))
                 break
         print (tabulate(results, headers=results_head, tablefmt="fancy_grid"))
-        db_store_history.add_search(f"WDC Standings after {race_name}")
+        db_store_history.add_search(f"{title} Standings after {race_name}")
 
     def go_back(self):
         menu.AdvancedMenu().run()
