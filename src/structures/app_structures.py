148cb96955553b1c26d38bf4543cfe2ec68aa207
Ethan Cornwill <ethancornwill@outlook.com>
2024-07-07 13:11:06 +1000

    refactor menus into classes

--- src/structures/app_structures.py
+++ src/structures/app_structures.py
@@ -1,6 +1,5 @@
 from colorama import Fore, Back
 
-from .standings import driver_standings, constructor_standings, race_results
 red = Fore.RED
 green = Fore.GREEN
 
@@ -9,45 +8,34 @@ import pyfiglet
 def ascii_title(t):
     return pyfiglet.figlet_format(t, font="smslant")
 
-def menu():
+
+
+class BaseMenu:
     """
-    This function displays a menu of options to the user and calls the respective
-    standings and race results menus based on the user's choice.
+    Base class for all menus in the app.
     """
-    print("Choose a category: \n")  # Display menu options
-    print(green + "1. Driver Standings")
-    print(green + "2. Team Standings")
-    print(green + "3. Race Results")
-    print(green + "4. Help")
-    print(green + "5. Exit")
-    
-    choice = input()  # Get user's choice
-    
-    # Match the user's choice and call the respective menu
-    match choice:
-        case "1":
-            title = ascii_title("Driver Standings")  # Display ASCII art title
-            print(red + title)
-            driver_standings.menu(ascii_title)
-        case "2":
-            title = ascii_title("Team Standings")
-            print(red + title)
-            constructor_standings.menu(ascii_title)
-        case "3":
-            title = ascii_title("Race Results")
-            print(red + title)
-            race_results.menu(ascii_title)
-            menu()  # Call menu again after race results are displayed
-        case "4":
-            title = ascii_title("Help")
-            print(red + title)
-            print("Help text here")
-            menu()
-        case "5":
-            print(red + "Goodbye!")  # Display exit message
-            exit()
-        case _:
-            print(Back.RED + "Invalid choice")  # Display error message for invalid choice
-            menu()
-            
+
+    def __init__(self):
+        pass
+
+    def display_menu(self):
+        """
+        Display the menu options.
+        """
+        raise NotImplementedError
+
+    def get_user_choice(self):
+        """
+        Get user's choice.
+        """
+        raise NotImplementedError
+
+    def call_menu_function(self, choice):
+        """
+        Call the respective menu function based on the user's choice.
+        """
+        raise NotImplementedError
+
+
+
 
